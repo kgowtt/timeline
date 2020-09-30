a8690f687449b46c3f5ea68e7ccc7b5115a1bbf9
APP技術部-林承聖 <eric@mitake.com.tw>
2020-09-30 18:03:34 +0800

    2020-0930練習

--- src/app/view/test/test.component.ts
+++ src/app/view/test/test.component.ts
@@ -1,3 +1,4 @@
+import { filter } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
-
+  hide = false;
+  xing = '';
   constructor() { }
 
+
+  idValue: string;
+  hideValue: string;
   ngOnInit(): void {
   }
+  onKey(value): void{
+    this.idValue = value;
+    this.hideValue = this.plusXing(this.idValue, 4 , 3 );
+    console.log(this.idValue);
+  }
+  plusXing(str: string, frontLen: number, endLen: number): string{
+    const len = str.length - frontLen - endLen;
+    let xing = '';
+    for (let i = 0; i < len; i ++) {
+      xing += '*';
+    }
+    return str.substring(0, frontLen) + xing + str.substring( str.length - endLen);
+  }
+
+  // psdFfun(): void{
+  //   console.log(12321213)
+  // }
+
 
 }
